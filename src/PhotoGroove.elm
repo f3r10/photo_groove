f36b947c21070ea8ba7e44b0e07c600ce54d9615
Fernando Ledesma <frledesma@outlook.com>
2021-10-12 20:47:23 -0500

    first attempt of default checked and onCheck event handler
    I am pretty sure that there should be a better option

--- src/PhotoGroove.elm
+++ src/PhotoGroove.elm
@@ -10,6 +10,7 @@ import Html exposing (button)
 import Html exposing (label)
 import Html exposing (input)
 import Html exposing (h3)
+import Html.Events exposing (onCheck)
 
 
 type ThumnailSize
@@ -18,8 +19,14 @@ type ThumnailSize
     | Large
 
 type alias Photo = {url : String}
-type alias Model =  { photos : List Photo, selectImg : String, chosenSize: ThumnailSize }
-type alias Msg = {description: String, data: String}
+type alias Model =  
+    { photos : List Photo
+    , selectImg : String
+    , chosenSize: (ThumnailSize, Bool) }
+type Msg 
+    = ClickedPhoto String
+    | ClickedSize ThumnailSize Bool
+    | ClickedSurpireseMe
 
 initialModel : Model 
 initialModel =
@@ -29,7 +36,7 @@ initialModel =
         , { url = "3.jpeg" }
         ]
     , selectImg = "1.jpeg"
-    , chosenSize = Large
+    , chosenSize = (Medium, True)
     }
 
 photoArray: Array Photo
@@ -47,12 +54,15 @@ view model =
     div [ class "content" ]
         [ h1 [] [ text "Photo Groove" ]
         , button
-            [onClick {description = "ClickedSurpireseMe", data = ""}]
+            [onClick ClickedSurpireseMe]
             [text "Surprise Me!"]
         , h3 [] [text "Thumnail Size:"]
         -- verbose code refactored to a Map
-        -- , div [id "choosen-size"] [viewSizeChooser Small, viewSizeChooser Medium, viewSizeChooser Large]
-        , div [id "choosen-size"] (List.map viewSizeChooser [Small, Medium, Large])
+        -- , div [id "choosen-size] [viewSizeChooser Small, viewSizeChooser Medium, viewSizeChooser Large]
+        , div [id "choosen-size"] 
+            (List.map 
+                (\s ->  viewSizeChooser s model.chosenSize) [Small, Medium, Large]
+            )
         , div [ id "thumbnails", class (sizeToClass model.chosenSize) ]
             -- (List.map (\url -> viewThumbnail model.selectImg url) model.photos)
             -- becuase of currying and partial application
@@ -64,9 +74,11 @@ view model =
             []
         ]
 
-sizeToClass : ThumnailSize -> String
+sizeToClass : (ThumnailSize, Bool) -> String
 sizeToClass size =
-    sizeToString size
+    let (s, _) = size 
+    in
+    sizeToString s
 
 
 viewThumbnail : String -> Photo -> Html Msg
@@ -79,14 +91,19 @@ viewThumbnail isSelected thumb =
                     == isSelected
               )
             ]
-        , onClick { description = "ClickPhoto", data = thumb.url }
+        , onClick (ClickedPhoto thumb.url)
         ]
         []
 
-viewSizeChooser : ThumnailSize -> Html Msg
-viewSizeChooser size =
+viewSizeChooser : ThumnailSize -> (ThumnailSize, Bool) -> Html Msg
+viewSizeChooser size ( choosenSize, selected ) =
     label []
-    [ input [type_ "radio", name "size"] []
+    [ input 
+        [type_ "radio"
+        , name "size"
+        -- , onClick (ClickedSize size)
+        , onCheck (\t -> ClickedSize size t)
+        , checked (if size == choosenSize then selected else False )] []
     , text (sizeToString size)
     ]
 
@@ -108,19 +125,13 @@ sizeToString size =
 
 update : Msg -> Model -> Model
 update msg model =
-    -- Refactored to a match case expression
-    -- if msg.description == "ClickPhoto" then
-    --     { model | selectImg = msg.data }
-    -- else if msg.description == "ClickedSurpireseMe" then
-    --     {model | selectImg = "2.jpeg"}
-    -- else
-    --     Model
-    case msg.description of
-        "ClickPhoto" -> 
-            { model | selectImg = msg.data }
-        "ClickedSurpireseMe" -> 
+    case msg of
+        ClickedPhoto url -> 
+            { model | selectImg = url }
+        ClickedSurpireseMe -> 
             {model | selectImg = "2.jpeg"}
-        _ -> model
+        ClickedSize size b -> 
+            {model |  chosenSize = (size, b)}
 
 
 main : Program () Model Msg
