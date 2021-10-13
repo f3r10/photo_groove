98d527a84bebf69580aadda718389f7a3df34349
Fernando Ledesma <frledesma@outlook.com>
2021-10-12 19:10:41 -0500

    Added helper functions and types for the next refactor

--- src/PhotoGroove.elm
+++ src/PhotoGroove.elm
@@ -4,8 +4,24 @@ import Html exposing (div, h1, img, text)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onClick)
 import Browser
+import Array exposing (Array)
+import Html exposing (Html)
+import Html exposing (button)
+import Html exposing (label)
+import Html exposing (input)
+import Html exposing (h3)
 
 
+type ThumnailSize
+    = Small
+    | Medium
+    | Large
+
+type alias Photo = {url : String}
+type alias Model =  { photos : List Photo, selectImg : String, chosenSize: ThumnailSize }
+type alias Msg = {description: String, data: String}
+
+initialModel : Model 
 initialModel =
     { photos =
         [ { url = "1.jpeg" }
@@ -13,17 +29,31 @@ initialModel =
         , { url = "3.jpeg" }
         ]
     , selectImg = "1.jpeg"
+    , chosenSize = Large
     }
 
+photoArray: Array Photo
+photoArray =
+    Array.fromList initialModel.photos
 
+
+urlPrefix : String
 urlPrefix =
     "http://elm-in-action.com/"
 
 
+view : Model -> Html Msg
 view model =
     div [ class "content" ]
         [ h1 [] [ text "Photo Groove" ]
-        , div [ id "thumbnails" ]
+        , button
+            [onClick {description = "ClickedSurpireseMe", data = ""}]
+            [text "Surprise Me!"]
+        , h3 [] [text "Thumnail Size:"]
+        -- verbose code refactored to a Map
+        -- , div [id "choosen-size"] [viewSizeChooser Small, viewSizeChooser Medium, viewSizeChooser Large]
+        , div [id "choosen-size"] (List.map viewSizeChooser [Small, Medium, Large])
+        , div [ id "thumbnails", class (sizeToClass model.chosenSize) ]
             -- (List.map (\url -> viewThumbnail model.selectImg url) model.photos)
             -- becuase of currying and partial application
             (List.map (viewThumbnail model.selectImg) model.photos)
@@ -34,7 +64,12 @@ view model =
             []
         ]
 
+sizeToClass : ThumnailSize -> String
+sizeToClass size =
+    sizeToString size
 
+
+viewThumbnail : String -> Photo -> Html Msg
 viewThumbnail isSelected thumb =
     img
         [ src (urlPrefix ++ thumb.url)
@@ -48,15 +83,47 @@ viewThumbnail isSelected thumb =
         ]
         []
 
+viewSizeChooser : ThumnailSize -> Html Msg
+viewSizeChooser size =
+    label []
+    [ input [type_ "radio", name "size"] []
+    , text (sizeToString size)
+    ]
 
+getPhotoUrl : Int -> String
+getPhotoUrl index = 
+    case Array.get index photoArray of
+        Just photo -> photo.url
+        Nothing -> ""
+
+
+sizeToString : ThumnailSize -> String
+sizeToString size =
+    case size of
+        Small -> "small"
+        Medium -> "med"
+        Large -> "large"
+
+
+
+update : Msg -> Model -> Model
 update msg model =
-    if msg.description == "ClickPhoto" then
-        { model | selectImg = msg.data }
+    -- Refactored to a match case expression
+    -- if msg.description == "ClickPhoto" then
+    --     { model | selectImg = msg.data }
+    -- else if msg.description == "ClickedSurpireseMe" then
+    --     {model | selectImg = "2.jpeg"}
+    -- else
+    --     Model
+    case msg.description of
+        "ClickPhoto" -> 
+            { model | selectImg = msg.data }
+        "ClickedSurpireseMe" -> 
+            {model | selectImg = "2.jpeg"}
+        _ -> model
 
-    else
-        model
 
-
+main : Program () Model Msg
 main =
     Browser.sandbox
     { init = initialModel
