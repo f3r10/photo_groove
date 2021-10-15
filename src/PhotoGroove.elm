8bfde8ced178eccedcdeee3f6dd9b08be8fb2dc8
Fernando Ledesma <frledesma@outlook.com>
2021-10-14 19:23:41 -0500

    Fixing format

--- src/PhotoGroove.elm
+++ src/PhotoGroove.elm
@@ -1,16 +1,10 @@
 module PhotoGroove exposing (main)
 
-import Html exposing (div, h1, img, text)
-import Html.Attributes exposing (..)
-import Html.Events exposing (onClick)
-import Browser
 import Array exposing (Array)
-import Html exposing (Html)
-import Html exposing (button)
-import Html exposing (label)
-import Html exposing (input)
-import Html exposing (h3)
-import Html.Events exposing (onCheck)
+import Browser
+import Html exposing (Html, button, div, h1, h3, img, input, label, text)
+import Html.Attributes exposing (..)
+import Html.Events exposing (onCheck, onClick)
 import Random
 
 
@@ -19,36 +13,48 @@ type ThumnailSize
     | Medium
     | Large
 
-type alias Photo = {url : String}
-type alias Size = {size: ThumnailSize, selected: Bool}
-type alias Model =  
+
+type alias Photo =
+    { url : String }
+
+
+type alias Size =
+    { size : ThumnailSize, selected : Bool }
+
+
+type alias Model =
     { photos : List Photo
-    , chosenSize: ThumnailSize
-    , sizes: List Size
-    , selectedUrl: String}
-type Msg 
+    , chosenSize : ThumnailSize
+    , sizes : List Size
+    , selectedUrl : String
+    }
+
+
+type Msg
     = ClickedPhoto String
     | ClickedSize Size
     | ClickedSurpireseMe
     | GotSelectedIndex Int
 
-initialModel : Model 
+
+initialModel : Model
 initialModel =
     { photos =
         [ { url = "1.jpeg" }
         , { url = "2.jpeg" }
         , { url = "3.jpeg" }
         ]
-        , chosenSize = Medium
-        , sizes = 
-            [{size = Small, selected = False}
-            , {size = Medium, selected = True}
-            , {size = Large, selected = False}
-            ]
-        , selectedUrl = "1.jpeg"
+    , chosenSize = Medium
+    , sizes =
+        [ { size = Small, selected = False }
+        , { size = Medium, selected = True }
+        , { size = Large, selected = False }
+        ]
+    , selectedUrl = "1.jpeg"
     }
 
-photoArray: Array Photo
+
+photoArray : Array Photo
 photoArray =
     Array.fromList initialModel.photos
 
@@ -63,11 +69,11 @@ view model =
     div [ class "content" ]
         [ h1 [] [ text "Photo Groove" ]
         , button
-            [onClick ClickedSurpireseMe]
-            [text "Surprise Me!"]
-        , h3 [] [text "Thumnail Size:"]
-        , div [id "choosen-size"] 
-            (List.map viewSizeChooser  model.sizes)
+            [ onClick ClickedSurpireseMe ]
+            [ text "Surprise Me!" ]
+        , h3 [] [ text "Thumnail Size:" ]
+        , div [ id "choosen-size" ]
+            (List.map viewSizeChooser model.sizes)
         , div [ id "thumbnails", class (sizeToClass model.chosenSize) ]
             (List.map (viewThumbnail model.selectedUrl) model.photos)
         , div [] (List.map printTuple model.sizes)
@@ -78,9 +84,11 @@ view model =
             []
         ]
 
+
 printTuple : Size -> Html Msg
 printTuple s =
-    Html.p [] [text (sizeToString s.size), text " -> ", text (Debug.toString s.selected)]
+    Html.p [] [ text (sizeToString s.size), text " -> ", text (Debug.toString s.selected) ]
+
 
 sizeToClass : ThumnailSize -> String
 sizeToClass s =
@@ -101,27 +109,39 @@ viewThumbnail isSelected thumb =
         ]
         []
 
+
 viewSizeChooser : Size -> Html Msg
 viewSizeChooser size =
     label []
-    [ input 
-        [type_ "radio"
-        , name "size"
-        -- , onClick (ClickedSize size)
-        , onCheck (\t -> ClickedSize {size | selected = t})
-        -- , checked (if size.size == chosenSize then True else False)
-        , checked size.selected] []
-    , text (sizeToString size.size)
-    ]
+        [ input
+            [ type_ "radio"
+            , name "size"
+
+            -- , onClick (ClickedSize size)
+            , onCheck (\t -> ClickedSize { size | selected = t })
+
+            -- , checked (if size.size == chosenSize then True else False)
+            , checked size.selected
+            ]
+            []
+        , text (sizeToString size.size)
+        ]
+
 
 getPhotoUrl : Int -> String
-getPhotoUrl index = 
+getPhotoUrl index =
     case Array.get index photoArray of
-        Just photo -> photo.url
-        Nothing -> ""
+        Just photo ->
+            photo.url
+
+        Nothing ->
+            ""
+
 
 
 -- TODO check appendix B for more information about elm packages
+
+
 randomPhotoPicker : Random.Generator Int
 randomPhotoPicker =
     Random.int 0 (Array.length photoArray - 1)
@@ -130,41 +150,56 @@ randomPhotoPicker =
 sizeToString : ThumnailSize -> String
 sizeToString size =
     case size of
-        Small -> "small"
-        Medium -> "med"
-        Large -> "large"
+        Small ->
+            "small"
 
+        Medium ->
+            "med"
+
+        Large ->
+            "large"
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        ClickedPhoto url -> 
+        ClickedPhoto url ->
             ( { model | selectedUrl = url }, Cmd.none )
-        ClickedSurpireseMe -> 
-            (model, Random.generate GotSelectedIndex randomPhotoPicker)
-        ClickedSize size -> 
-            let updateSizes =
-                    List.map 
-                        (\s -> 
-                            if s.size == size.size then size  else {size = s.size,
-                            selected = False}
-                        ) model.sizes
-            in 
-            ({model |  chosenSize = size.size, sizes = updateSizes}, Cmd.none)
 
-        GotSelectedIndex index -> 
-            ({model | selectedUrl = getPhotoUrl index}, Cmd.none)
+        ClickedSurpireseMe ->
+            ( model, Random.generate GotSelectedIndex randomPhotoPicker )
+
+        ClickedSize size ->
+            let
+                updateSizes =
+                    List.map
+                        (\s ->
+                            if s.size == size.size then
+                                size
+
+                            else
+                                { size = s.size
+                                , selected = False
+                                }
+                        )
+                        model.sizes
+            in
+            ( { model | chosenSize = size.size, sizes = updateSizes }, Cmd.none )
+
+        GotSelectedIndex index ->
+            ( { model | selectedUrl = getPhotoUrl index }, Cmd.none )
+
 
 
 -- () means no flags whatsoever. What flags will do?
+
+
 main : Program () Model Msg
 main =
     Browser.element
-    -- The second parameter, the command, will run when the page loads
-    { init = \flags ->  ( initialModel, Cmd.none )
-    , view = view
-    , update = update
-    , subscriptions = \model -> Sub.none
-    }
-    
+        -- The second parameter, the command, will run when the page loads
+        { init = \flags -> ( initialModel, Cmd.none )
+        , view = view
+        , update = update
+        , subscriptions = \model -> Sub.none
+        }
